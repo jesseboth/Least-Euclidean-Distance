81026ef03a7cb12755f282f26a441a25db17bf15
jesseboth <jessebot@buffalo.edu>
2020-11-04 11:35:55 -0500

    Adjustment for testing

--- Project/main.s
+++ Project/main.s
@@ -3,14 +3,23 @@
 .globl main
 .text
     main:
+        # la $a0 filename
+        # jal load_points
+        # move $a0, $v0
+        # move $a1, $v1
+        # jal sort_points_by_x
+        # jal find_closest
+        # move $a0, $v0
+        # move $a1, $v1
+        # jal output_cloest_pair
+        # li $v0 10
+        # syscall
+#----------------------------------------------------#
+        j test
+
+    test:
         la $a0 filename
         jal load_points
-        move $a0, $v0
-        move $a1, $v1
-        jal sort_points_by_x
-        jal find_closest
-        move $a0, $v0
-        move $a1, $v1
-        jal output_cloest_pair
+
         li $v0 10
         syscall
