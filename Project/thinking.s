7756c20aba66282278bb218856ae08147ae6a930
jesseboth <jessebot@buffalo.edu>
2020-11-10 17:38:31 -0500

    worked on find closest

--- Project/thinking.s
+++ Project/thinking.s
@@ -84,93 +84,137 @@ load_points_helper:
 
 
     ################################
-####################
-#old working
-    move $t8, $a0
-    # reading from file just opened
-    li   $v0, 14        #read
-    la   $a1, buffer    # address
-    li   $a2,  4        # length to get num_points
-    syscall            
 
-    # saving num_points
-    la  $t0, buffer     #location
-    lw $t1, 0($t0)      #save numpoints
-    sw $t1, num_points  #store value
+find_closest:
 
-    add $s0, $zero, $t1 #save numpoints
-    sll $s0, $s0, 3     #numpoints * 2 points * 4 bits
-    addi $s0, $s0, 4
-    sw $s0, buffer      #change buffer for the required space
-    addi $s0, $s0, -4
+    #---------------------------------------------------------------#
+    #Euclidean:
+        # $a0 is x0 $a1 is y0; a2 is x1 a3 is y1
+        # $v0 is the calculated distance
+        #Meaning that these values need to be overwritten
     
-    #print numpoints
-    lw $a0, 0($t0)
-    li  $v0, 1          #print int
-    syscall
+    #Find:
+        #Need to store the argument values so they are not lost
+        #$s0 = x0 (return value) *(x0 + 4 = y0)
+        #$s1 = x1 (return value)
+        #$s2 = least
+    #---------------------------------------------------------------#
 
-    li $a0, 0xA        #new line
-    li $v0, 11
-    syscall  
+    move $s7, $read         #store return
 
-    # read from file with given length
-    li   $v0, 14        # read
-    move $a0, $t8
-    la   $a1, buffer    # address
-    move   $a2,  $s0    # length = numpoints * 2 points * 4 bits
-    syscall
+    move $t0, $a0           #num_points
+    move $t1, $a1           #base_address
+    move $t2, $t1           #point 2
+    move $t3, $a0           #count
+    addi $t3, $t3, -1       #decrementer
 
-    #allocate space
-    li $v0 9          #sbrk
-    move $a0, $s0
-    syscall
+    beq $s2, $zero, max      #if(s2 == 0)   
 
-    # resave address
-    la $t0, buffer     #location
-    move $t1, $v0        #location of sbrk
-
-    move $t8, $v0        #store for return
-
-    lw $s1, num_points
-    li $t2, 0
     loop:
-        beq $t2, $s1, exit
+        beq $t2, $zero, recurse
+        addi $t2, $t2, 8    #to next point
+        addi $t2, $t2, -1   #decrement for next iter
+        j find_euclidean
 
-        #x
-        lw $t3, 0($t0)
-        sw $t3, 0($t1)
-        #y
-        lw $t3, 4($t0)
-        sw $t3, 4($t1)
+    find_euclidean:
+        sw $a0, 0($t1)      #x0
+        sw $a1, 4($t1)      #y0
+        sw $a2, 0($t2)      #x1
+        sw $a3, 4($t2)      #y1
+        jal euclidean_distance
 
+        #check if d < least
+        blt $v0, $s2, new_least
+        j loop
 
-        # print points
-        lw $a0, 0($t1)
-        li  $v0, 1
-        syscall
-        li $a0, 0x3B        #;
-        li $v0, 11
-        syscall
-        lw $a0, 4($t1)
-        li  $v0, 1
-        syscall   
-        li $a0, 0xA        #new line
-        li $v0, 11
-        syscall   
+    new_least:
+        move $s0, $a0       #store x0
+        move $s1, $a2       #store x1
+        move $s2, $v0       #store least
 
-        #to next address
-        addi $t0, $t0, 8
-        addi $t1, $t1, 8
+        #exit if distance = 0
+        beq $s2, $zero, exit 
+        j loop
 
-        addi $t2, $t2, 1
-        j loop 
-    
+    recurse:
+        addi $a0, $t0, -1   #num_points -1
+        sw $a1, 8($t1)      #next point
+        move $ra, $s7       #save return
+        j find_closest
+
+    max:
+        li $s2, 0xEF4201    #2800^2 + 2800^2 +1
+        j loop
+
     exit:
-        lw $v0, num_points
-        move $v1, $t8
+        move $ra, $s7
+        move $v0, $s0
+        move $v1, $s1
 
-    li $a0, 0x23        #new line
-    li $v0, 11
-    syscall 
+########################
+old_find_closest:
+    move $t0, $a0 #num_points
+    move $t1, $a1 #base_address
 
-####################
+    li $t3, 8 #index
+    sw $t4, 0($t1) #store for second point
+    li $t5, 1 #count (numpoints -1)
+
+    beq $s2, $zero max #if(s2 == 0)
+    
+    #check if done
+    addi $t2, $zero, 1
+    beq $a0, $t2, exit
+
+    #while loop
+    j loop
+
+    loop:
+        beq $t5, $t0, recurse #if t5 == t0 -> if count == num_points
+
+        sw $t4, 8($t4) #next point
+
+        #store for next iteration
+        addi $t3, $t3, 8 #next pair index
+        addi $t5, $t5, 1 #count +1
+        
+        j find_euclidean #check base_address and next point
+
+
+    find_euclidean:
+        sw $a0, 0($t1)  #x0
+        sw $a1, 4($t1)  #y0
+        sw $a2, 0($t4)  #x1
+        sw $a3, 4($t4) #y1
+        jal euclidean_distance
+
+        #check if d < least
+        blt $v0, $s2, new_least
+
+        j loop
+
+    new_least:
+        move $s0, $a0 #store x0
+        move $s1, $a2 #store x1
+        move $s2, $v0 #store least
+
+        beq $s2, $zero, exit #exit if distance = 0
+
+        j loop
+        
+
+    recurse:
+        #set arguments
+        addi $a0, $t0, -1
+        sw $a1, 8($t1) #to next x,y
+        j find_closest
+        
+    max:
+        #stores the greatest value least can be
+        li $s2, 0xEF4201
+        j find_euclidean
+
+    exit:
+        #store the return values
+        move $v0, $s0
+        move $v1, $s1
