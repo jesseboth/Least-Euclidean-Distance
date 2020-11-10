a152d4111132ae75bb57e9d7ff0a367a6aae0f40
jesseboth <jessebot@buffalo.edu>
2020-11-10 17:35:29 -0500

    Working and correct

--- Project/find.s
+++ Project/find.s
@@ -7,7 +7,6 @@ find_closest:
     # i.e., $v0 is the address of x0 in the array, $v1 is the address of x1 in the array
 
     #####  put your codes below this line #####
-
     #---------------------------------------------------------------#
     #Euclidean:
         # $a0 is x0 $a1 is y0; a2 is x1 a3 is y1
@@ -18,88 +17,63 @@ find_closest:
         #Need to store the argument values so they are not lost
         #$s0 = x0 (return value) *(x0 + 4 = y0)
         #$s1 = x1 (return value)
-        #$s2 = least
-        #$t0 = num_points
-        #$t1 = baseaddress
-
+        #$s4 = least
     #---------------------------------------------------------------#
 
-    move $t0, $a0 #num_points
-    move $t1, $a1 #base_address
+    move $s7, $ra           #store return
 
-    li $t3, 8 #index
-    sw $t4, 0($t1) #store for second point
-    li $t5, 1 #count (numpoints -1)
+    move $t0, $a0           #num_points
+    move $t1, $a1           #base_address
+    move $t2, $a1           #point 2
+    addi $t3, $a0, -2       #decrementer
 
-    beq $s2, $zero max #if(s2 == 0)
-    
-    #check if done
-    addi $t2, $zero, 1
-    beq $a0, $t2, exit
-
-    #while loop
-    j loop
+    beq $s4, $zero, max      #if(s == 0)
+    li $t5, 1
+    beq $t0, $t5, exit   
 
     loop:
-        beq $t5, $t0, recurse #if t5 == t0 -> if count == num_points
-
-        sw $t4, 8($t4) #next point
-
-        #store for next iteration
-        addi $t3, $t3, 8 #next pair index
-        addi $t5, $t5, 1 #count +1
-        
-        j find_euclidean #check base_address and next point
-
+        beq $t3, $zero, recurse
+        addi $t2, $t2, 8    #to next point
+        addi $t3, $t3, -1   #decrement for next iter
+        j find_euclidean
 
     find_euclidean:
-        sw $a0, 0($t1)  #x0
-        sw $a1, 4($t1)  #y0
-        sw $a2, 0($t4)  #x1
-        sw $a3, 4($t4) #y1
+        lw $a0, 0($t1)      #x0
+        lw $a1, 4($t1)      #y0
+        lw $a2, 0($t2)      #x1
+        lw $a3, 4($t2)      #y1
         jal euclidean_distance
 
         #check if d < least
-        blt $v0, $s2, new_least
-
+        blt $v0, $s4, new_least
         j loop
 
     new_least:
-        move $s0, $a0 #store x0
-        move $s1, $a2 #store x1
-        move $s2, $v0 #store least
+        move $s0, $t1       #store x0
+        move $s1, $t2       #store x1
+        move $s4, $v0       #store least
 
-        beq $s2, $zero, exit #exit if distance = 0
-
+        #exit if distance = 0
+        beq $s4, $zero, exit 
         j loop
-        
 
     recurse:
-        #set arguments
-        addi $a0, $t0, -1
-        sw $a1, 8($t1) #to next x,y
+        addi $a0, $t0, -1   #num_points -1
+        addi $a1, $t1, 8    #next point
+        move $ra, $s7       #save return
         j find_closest
-        
+
     max:
-        #stores the greatest value least can be
-        li $s2, 0xEF4200
-        j find_euclidean
+        li $s4, 15680001    #2800^2 + 2800^2 +1
+        j loop
 
     exit:
-        #store the return values
+
+
+        move $ra, $s7
         move $v0, $s0
         move $v1, $s1
 
-    #------------------------------------------------------------#
-    # j test
-    # test:
-    #     li $v0, 4
-	#     la $a0, hello
-	#     syscall
-
-	#     li $v0,10
-	#     syscall
-    #------------------------------------------------------------#
     
     #####  put your codes above this line #####
-    # jr $ra
+    jr $ra
