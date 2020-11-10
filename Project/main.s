4b0f5fe6a4a28192acba7c8ab8da36e498995a96
jesseboth <jessebot@buffalo.edu>
2020-11-10 15:38:21 -0500

    print points

--- Project/main.s
+++ Project/main.s
@@ -20,6 +20,52 @@
     test:
         la $a0 filename
         jal load_points
+        move $a0, $v0
+        move $a1, $v1
+        jal print
 
         li $v0 10
         syscall
+
+    print:
+        ################################
+        move $s1, $a0
+        move $t0, $a1
+        li $t2, 0
+
+        #print num_points
+        li $v0, 1                     
+        syscall
+        li $a0, 0xA        #new line
+        li $v0, 11
+        syscall
+
+        loop:
+            beq $t2, $s1, exit
+            # print points
+            lw $a0, 0($t0)
+            li  $v0, 1
+            syscall
+            li $a0, 0x3B        #;
+            li $v0, 11
+            syscall
+            lw $a0, 4($t0)
+            li  $v0, 1
+            syscall   
+            li $a0, 0xA        #new line
+            li $v0, 11
+            syscall   
+
+            #to next address
+            addi $t0, $t0, 8
+
+            addi $t2, $t2, 1
+            j loop 
+
+        exit:
+
+        li $a0, 0x23        #new line
+        li $v0, 11
+        syscall
+        jr $ra
+        ###############################
