6dce892a2a78385fb8ab971126a7ba5c16d943c8
jesseboth <jessebot@buffalo.edu>
2020-11-14 18:48:11 -0500

    Working Sort

--- Project/sort.s
+++ Project/sort.s
@@ -5,149 +5,62 @@ sort_points_by_x:
     # note that array has the order as x0, y0, x1, y1, x2, y3, ..., xn , yn
  
     #####  put your codes below this line #####
-    # move $s6, $a0
 
-    li $t8, 0               #sorted length
-    sll $s7, $a0, 3         #length
-    addi $t9, $s7, -8       #length -1 (stop)
+    move $s0, $a0       #num_points
+    move $s1, $a1       #base_address
+    li $s2, 1           #end
+    
+    j sort_x
 
-    #i
-    li $t0, 0               #i 
-    move $s0, $a1           #array[i]
 
-    #current pos
-    li $t1, 8               #current pos
-    addi $s1, $a1, 8        #array[current]
+    sort_x:
+        beq $s0, $s2, exit_x #if(num_points == 1){exit}
+        move $t0, $s0       #num_points
+        move $t1, $s1       #base_address
+        move $t2, $s1       #checking
+        addi $t3, $s0, -1   #decrementer
 
-    li $s2, 0               #check pos
+        j loop_x
 
-        ################
-        li $a0, 0x3B        #new line
-        li $v0, 11
-        syscall  
-        ################
+    loop_x:
+        beq $t3, $zero, recurse_x
+        addi $t2, $t2, 8        #to next point
+        addi $t3, $t3, -1       #decrement
 
-    while:
-        beq $t8, $t9, end  #while(sort_length < length-2)
+        j compare_x
 
-        add $s0, $a1, $s2       #array[i]
-        move $t0, $s2       #i = check_pos
+    compare_x:
+        lw $t4, 0($t1)      #x0
+        lw $t5, 0($t2)      #x1
 
-        # li $v0, 1
-        # move $a0, $t0
-        # syscall
-        #################
-        # li $v0, 1
-        # lw $a0, 0($s0)
-        # syscall
-        # li $a0, 0xA        #new line
-        # li $v0, 11
-        # syscall  
-        # li $v0, 1
-        # lw $a0, 8($s0)
-        # syscall
-        # li $a0, 0xA        #new line
-        # li $v0, 11
-        # syscall 
-        # li $v0, 1
-        # lw $a0, 16($s0)
-        # syscall
-        # li $a0, 0xA        #new line
-        # li $v0, 11
-        # syscall 
-        # li $v0, 1
-        # lw $a0, 24($s0)
-        # syscall
-        # li $a0, 0xA        #new line
-        # li $v0, 11
-        # syscall
-        # li $a0, 0x3B        #new line
-        # li $v0, 11
-        # syscall
-        # li $a0, 0xA        #new line
-        # li $v0, 11
-        # syscall   
-        #################
-        j while2
-    while2:
-    ###################
-        # li $v0, 1
-        # move $a0, $t0
-        # syscall
-    ###################
+        #if(x1 < x0)
+        blt $t5, $t4, swap_x
+        j loop_x
 
-        bgt $t0, $t1, while #while(i <= current_pos):
+    swap_x:
+        lw $t6, 4($t1) #y0
+        lw $t7, 4($t2) #y1
+
+        #old least
+        sw $t4, 0($t2)
+        sw $t6, 4($t2)
+
+        #new least
+        sw $t5, 0($t1)
+        sw $t7, 4($t1)
+        j loop_x
+
+    recurse_x:
+
+        addi $s0, $t0, -1   #num_points -1
+        addi $s1, $t1, 8    #next point
+        j sort_x
+
         
-        lw $t2, 0($s0)      #value of array[i]
-        lw $t3, 0($s1)      #value of array[current]
 
-        blt $t3, $t2, swap #if(array[current_pos] < array[i])
-        beq $t0, $t1, if_helper #if(i == current_pos and i != length)
+    exit_x:
 
-        addi $t0, $t0, 8    #i+=2
-        addi $s1, $s1, 8
-
-        j while2
-
-    swap:
-
-        #point in i
-        lw $t2, 0($s0)
-        lw $t3, 4($s0)
-
-        #point in current
-        lw $t4, 0($s1)
-        lw $t5, 4($s1)
-
-        #array[i] = array[current_pos]
-        sw $t2, 0($s1)
-        sw $t3, 4($s1)
-
-        #array[current_pos] = array[i]
-        sw $t4, 0($s0)
-        sw $t5, 4($s0)
-
-        move $s2, $t0   #check_pos = i
-
-        ###################
-        li $v0, 1
-        move $a0, $t0
-        syscall
-        ###################
-
-        j while
-
-    if_helper:
-        bne $t0, $s7, inc
-
-        #increment i
-        addi $t0, $t0, 8
-        addi $s0, $s0, 8
-
-        j while2    #continue inner loop
-
-
-    inc:
-        #################
-        # li $v0, 1
-        # lw $a0 0($s0)
-        # syscall
-        # li $a0, 0xA        #new line
-        # li $v0, 11
-        # syscall  
-        #################
-
-        addi $t8, $t8, 8    #sorted length
-        addi $t1, $t1, 8    #current pos
-        addi $s1, $s1, 8
-        li $s2, 0
-
-        j while
-
-    end:
-        # move $v0, $a0
-        li $v0, 20          #TODO change
-        move $v1, $a1
+    #no return (v0)
     #####  put your codes above this line #####
     jr $ra
 
@@ -158,8 +71,63 @@ sort_points_by_y:
  
     #####  put your codes below this line #####
 
+    move $s0, $a0       #num_points
+    addi $s1, $a1, 4       #base_address
+    li $s2, 1           #end
+    
+    j sort_y
+
+
+    sort_y:
+        beq $s0, $s2, exit_y #if(num_points == 1){exit}
+        move $t0, $s0       #num_points
+        move $t1, $s1       #base_address
+        move $t2, $s1       #checking
+        addi $t3, $s0, -1   #decrementer
+
+        j loop_y
+
+    loop_y:
+        beq $t3, $zero, recurse_y
+        addi $t2, $t2, 8        #to next point
+        addi $t3, $t3, -1       #decrement
+
+        j compare_y
+
+    compare_y:
+        lw $t4, 0($t1)      #y0
+        lw $t5, 0($t2)      #y1
+
+        #if(x1 < x0)
+        blt $t5, $t4, swap_y
+        j loop_y
+
+    swap_y:
+        lw $t6, -4($t1) #x0
+        lw $t7, -4($t2) #x1
+
+        #old least
+        sw $t4, 0($t2)
+        sw $t6, -4($t2)
+
+        #new least
+        sw $t5, 0($t1)
+        sw $t7, -4($t1)
+        j loop_y
+
+    recurse_y:
+
+        addi $s0, $t0, -1   #num_points -1
+        addi $s1, $t1, 8    #next point
+        j sort_y
+
+        
+
+    exit_y:
 
 
     
     #####  put your codes above this line #####
     jr $ra
+
+    ######################################################        
